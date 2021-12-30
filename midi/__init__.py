a6fe037055180dd0af529568ca40eaf8a7cdbb08
kaparoo <kaparoo2001@gmail.com>
2021-12-30 16:36:55 +0900

    :sparkles: Add linear decaying function

--- midi/__init__.py
+++ midi/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from .constant import MAX_MIDI_KEY, MIN_MIDI_KEY, NUM_MIDI_KEYS, NUM_PITCH_CLASSES
-    from .decay_fn import DecayFn
-    from .unit import MIDIRest, MIDIUnit, MIDIUnitSequence, MIDIUnitSequenceList
-except ImportError:
     from constant import MAX_MIDI_KEY, MIN_MIDI_KEY, NUM_MIDI_KEYS, NUM_PITCH_CLASSES
-    from decay_fn import DecayFn
+    from decay_fn import DecayFn, get_decay
     from unit import MIDIRest, MIDIUnit, MIDIUnitSequence, MIDIUnitSequenceList
+except ImportError:
+    from .constant import MAX_MIDI_KEY, MIN_MIDI_KEY, NUM_MIDI_KEYS, NUM_PITCH_CLASSES
+    from .decay_fn import DecayFn, get_decay
+    from .unit import MIDIRest, MIDIUnit, MIDIUnitSequence, MIDIUnitSequenceList
 
 __all__ = [
     "MAX_MIDI_KEY",
@@ -15,6 +15,7 @@ __all__ = [
     "NUM_MIDI_KEYS",
     "NUM_PITCH_CLASSES",
     "DecayFn",
+    "get_decay",
     "MIDIRest",
     "MIDIUnit",
     "MIDIUnitSequence",
--- midi/decay_fn.py
+++ midi/decay_fn.py
@@ -3,4 +3,36 @@
 import numpy as np
 from typing import Callable
 
+__all__ = ["DecayFn", "get_decay"]
+
+
 DecayFn = Callable[[np.ndarray], np.ndarray]
+
+
+def linear_decay_factory(settling_frame: int = 10) -> DecayFn:
+    def linear_decay(array: np.ndarray) -> np.ndarray:
+        decayed_array = np.zeros_like(array, dtype=np.uint8)
+        prev_pressed, prev_velocity = False, 0
+        for idx in range(len(array)):
+            curr_velocity = 0
+            if array[idx] <= 0:
+                prev_pressed = False
+            elif idx > 0:
+                prev_velocity = decayed_array[idx - 1]
+                if prev_velocity > 0:
+                    curr_velocity = prev_velocity - 1
+                elif not prev_pressed:
+                    curr_velocity = settling_frame - 1
+                prev_pressed = True
+            else:
+                curr_velocity = settling_frame - 1
+                prev_pressed = True
+            decayed_array[idx] = curr_velocity
+        return decayed_array
+
+    return linear_decay
+
+
+def get_decay(name: str = "linear", settling_frame: int = 10) -> DecayFn:
+    if name == "linear":
+        return linear_decay_factory(settling_frame)
